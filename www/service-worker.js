eb393240a8b0cbd1f7fcbdf1f5a9f2691756963e
Matt Halstead <m.russell.halstead@gmail.com>
2018-04-11 22:34:41 -0400

    Big changes.

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
  * more info on how to use sw-toolbox to custom configure your service worker.
@@ -28,35 +27,4 @@ self.toolbox.router.any('/*', self.toolbox.cacheFirst);
 
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
-=======
-/**
- * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
- * more info on how to use sw-toolbox to custom configure your service worker.
- */
-
-
-'use strict';
-importScripts('./build/sw-toolbox.js');
-
-self.toolbox.options.cache = {
-  name: 'ionic-cache'
-};
-
-// pre-cache our key assets
-self.toolbox.precache(
-  [
-    './build/main.js',
-    './build/main.css',
-    './build/polyfills.js',
-    'index.html',
-    'manifest.json'
-  ]
-);
-
-// dynamically cache any other local assets
-self.toolbox.router.any('/*', self.toolbox.cacheFirst);
-
-// for any other requests go to the network, cache,
-// and then only use that cached resource if your user goes offline
->>>>>>> f350ab144518c0a061a93df3e6c24ce7be28c9c6
 self.toolbox.router.default = self.toolbox.networkFirst;
